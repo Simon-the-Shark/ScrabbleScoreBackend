cc2b227a8480f2d20ac11a707f93adb1c2e91a85
simon-the-shark <simon_rekin@o2.pl>
2020-07-23 01:00:33 +0200

    added zipped db download endpoint

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import os.path
 import sqlite3
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, send_from_directory
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
 
 @app.errorhandler(404)
@@ -13,10 +13,15 @@ def not_found(error):
 
 @app.route("/api/v0/sjp/<string:word>", methods=['GET'])
 def word_view(word):
-    conn = sqlite3.connect("sjp-20200717.db")
+    conn = sqlite3.connect(os.path.join("dbs", "sjp-20200717.db"))
     cursor = conn.cursor()
     result = cursor.execute("SELECT COUNT(*) FROM words WHERE word = '{}' LIMIT 1".format(word)).fetchall()
     approved = result[0][0] > 0
     cursor.close()
     conn.close()
     return jsonify({"approved": approved})
+
+
+@app.route('/download/sjp-20200717.zip', methods=['GET'])
+def download():
+    return send_from_directory(directory=os.path.join(app.root_path, "dbs"), filename="sjp-20200717.zip")
